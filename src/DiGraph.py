4a95bb63377956615bc09bd2d0e12ddb2d76a3f9
yuvalbenya <yuvime12@gmail.com>
2020-12-30 15:39:49 +0200

    first

--- src/DiGraph.py
+++ src/DiGraph.py
@@ -6,33 +6,39 @@ class DiGraph(GraphInterface):
         self.V = {}
         self.inE ={}
         self.outE ={}
-
-
+        self.mc = 0
+        self.esize = 0
 
     def v_size(self):
-        pass
+        return self.V.__sizeof__()
 
     def e_size(self):
-        pass
+        return self.esize
 
     def get_mc(self):
-        pass
+        return self.mc
 
     def add_edge(self, id1, id2, weight):
         pass
 
     def add_node(self, node_id, pos=None):
-        self.V.update({node_id: pos})
-        print(self.V)
+        if not(self.V.__contains__(node_id)):
+            self.V.update({node_id: pos})
+            self.inE.update({node_id: {}})
+            self.outE.update({node_id: {}})
+
     def remove_node(self, node_id):
         pass
 
+
     def remove_edge(self, node_id1, node_id2):
         pass
+
     def getV(self) -> dict :
-        print(self.V)
+        return(self.V)
 
 
 if __name__ == '__main__':
-        d = DiGraph
-
+    d = DiGraph()
+    d.add_node(0,(33,423,234))
+    print(d.getV())
